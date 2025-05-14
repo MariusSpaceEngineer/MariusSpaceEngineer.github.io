6a914c5990c1d04bdd25e569cf68c022fa893dbb
Marius Stoica <stoicamarius9010@gmail.com>
2025-05-15 00:17:07 +0200

    Add portofolio html as index.html + rename index.md to blog.md

---
+++ script.js
@@ -0,0 +1,20 @@
+const recipientEmail = "stoicamarius9010@gmail.com";
+
+document.addEventListener("DOMContentLoaded", function () {
+  sendMail(recipientEmail);
+});
+
+const sendMail = (recipientEmail) => {
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const senderName = document.getElementById("senderName").value;
+      const senderEmail = document.getElementById("senderEmail").value;
+      const message = document.getElementById("message").value;
+
+      const mailtoLink = `mailto:${recipientEmail}?subject=Message from ${senderName} via Portfolio Website&body=${message} %0D%0A${senderName}%0D%0A${senderEmail}`;
+      window.location.href = mailtoLink;
+    });
+};
